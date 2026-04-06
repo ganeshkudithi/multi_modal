c1d4b777212a66f0dc7126feae2d82032cb26eb4
gANI <164795967+ganeshkudithi@users.noreply.github.com>
2026-04-06 14:37:45 +0530

    Implement MicroDataset class for image-text pairs

--- dataset.py
+++ dataset.py
@@ -1,1 +1,26 @@
+import json
+from PIL import Image
+from torch.utils.data import Dataset
+from torchvision import transforms
 
+class MicroDataset(Dataset):
+    def __init__(self, json_path):
+        with open(json_path) as f:
+            self.data = json.load(f)
+
+        self.transform = transforms.Compose([
+            transforms.Resize((512, 512)),
+            transforms.ToTensor()
+        ])
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        item = self.data[idx]
+
+        text = item["text_input"]
+        image = Image.open(item["image"]).convert("RGB")
+        image = self.transform(image)
+
+        return text, image
