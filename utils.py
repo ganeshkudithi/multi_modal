ddeaf0a443696cfc620e8c58834247060680d976
gANI <164795967+ganeshkudithi@users.noreply.github.com>
2026-04-06 14:40:48 +0530

    Add VAEWrapper class for image encoding and decoding

--- utils.py
+++ utils.py
@@ -1,1 +1,17 @@
+from diffusers import AutoencoderKL
+import torch
 
+class VAEWrapper:
+    def __init__(self):
+        self.vae = AutoencoderKL.from_pretrained(
+            "stabilityai/sd-vae-ft-mse"
+        ).cuda()
+
+    def encode(self, image):
+        image = image.unsqueeze(0).cuda()
+        latent = self.vae.encode(image).latent_dist.sample()
+        return latent
+
+    def decode(self, latent):
+        image = self.vae.decode(latent).sample
+        return image
